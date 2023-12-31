869f5a6628c7bb76dd2383f523ca298ede19b2c6
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-01-01 01:52:10 +0300

    Main | update

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000';
+  const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
 
   const options = new DocumentBuilder()
-    .setTitle('Your API Title')
-    .setDescription('Your API description')
+    .setTitle("Your API Title")
+    .setDescription("Your API description")
     .addBearerAuth()
-    .setVersion('1.0.0')
-    .addBearerAuth(undefined, 'defaultBearerAuth')
+    .setVersion("1.0.0")
+    .addBearerAuth(undefined, "defaultBearerAuth")
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: corsOrigin,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-});
-  await app.listen(3333);
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
